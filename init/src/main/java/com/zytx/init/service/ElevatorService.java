46fd01e28d82e1e22612ad0d83a23ead06f997ba
侯若飞 <houruofei_lovexinh@163.com>
2018-12-11 11:46:30 +0800

    合并分支的代码

--- init/src/main/java/com/zytx/init/service/ElevatorService.java
+++ init/src/main/java/com/zytx/init/service/ElevatorService.java
@@ -14,8 +14,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
